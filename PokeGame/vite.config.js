6fd3c6ab7ccdb18a5d8ea34e5f3993f4b3b1031e
Austin Flores <austin3flores@gmail.com>
2025-05-29 20:44:05 -0700

    updated .env

--- PokeGame/vite.config.js
+++ PokeGame/vite.config.js
@@ -1,23 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv'
-import { spawn } from 'child_process'
-
-dotenv.config()
-
-let serverProcess = null
 
 // Documentation: https://vitejs.dev/config/
-export default defineConfig({
-  base: './',
-  plugins: [react()],
-  esbuild: {
-    loader: "jsx"
-  },
-  assetsInclude: ['**/*.glb'],
-  server: {
-    // Use VITE_PORT from your .env, or default to a port if not specified
-    port: parseInt(process.env.VITE_PORT, 10) || 3000,
-    host: process.env.NODE_ENV === 'development' ? 'localhost': '0.0.0.0',
-  }
-})
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return defineConfig({
+    base: './',
+    plugins: [react()],
+    esbuild: {
+      loader: "jsx"
+    },
+    assetsInclude: ['**/*.glb'],
+    server: {
+      // Use VITE_PORT from your .env, or default to a port if not specified
+      port: parseInt(env.VITE_PORT, 10) || 3000,
+      host: env.NODE_ENV === 'development' ? 'localhost': '0.0.0.0',
+    }
+  });
+};
